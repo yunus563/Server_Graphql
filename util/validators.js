4dd2622037bf6036c1144f498e01d5e04d57be68
Yunus <59920062+yunus563@users.noreply.github.com>
2021-09-15 18:17:09 +0500

    asd
    asd

--- util/validators.js
+++ util/validators.js
@@ -6,20 +6,20 @@ module.exports.validateRegisterInput = (
 ) => {
   const errors = {};
   if (username.trim() === '') {
-    errors.username = 'Username must not be empty';
+    errors.username = 'Username yozilmagan...!';
   }
   if (email.trim() === '') {
-    errors.email = 'Email must not be empty';
+    errors.email = 'Email yozilmagan...!';
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
     if (!email.match(regEx)) {
-      errors.email = 'Email must be a valid email address';
+      errors.email = 'Email example@gmail.com nusxada bolishi kerak';
     }
   }
   if (password === '') {
     errors.password = 'Password must not empty';
   } else if (password !== confirmPassword) {
-    errors.confirmPassword = 'Passwords must match';
+    errors.confirmPassword = 'Password yozilmagan';
   }
 
   return {
@@ -31,10 +31,10 @@ module.exports.validateRegisterInput = (
 module.exports.validateLoginInput = (username, password) => {
   const errors = {};
   if (username.trim() === '') {
-    errors.username = 'Username must not be empty';
+    errors.username = 'Username yozilmagan...!';
   }
   if (password.trim() === '') {
-    errors.password = 'Password must not be empty';
+    errors.password = 'Password  yozilmagan...!';
   }
 
   return {
